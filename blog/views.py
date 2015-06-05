7596c92471aa7754e9b92b5262166674e4cc21a5
rcini <riccardo.cini@playnet.it>
2015-06-05 16:59:23 +0200

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,4 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {})
